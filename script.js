46a8cc88601953e5e543e3df4c2600309029fc22
Deepanshi Soni <106514031+soniDeepanshi@users.noreply.github.com>
2022-05-31 20:22:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+var quoteText = document.querySelector(".quote"),
+    quoteBtn = document.querySelector("button"),
+    authorName = document.querySelector(".name"),
+    speechBtn = document.querySelector(".speech"),
+    copyBtn = document.querySelector(".copy"),
+    twitterBtn = document.querySelector(".twitter"),
+    synth = speechSynthesis;
+
+function randomQuote() {
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading...";
+    fetch("https://api.quotable.io/random").then(response => response.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "Generate Quote";
+    });
+}
+
+copyBtn.addEventListener("click", () => {
+    var r = document.createRange();
+    r.selectNode(document.getElementById("quote"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    document.getElementById("copy-msg").classList.add("copied-box");
+    setTimeout(function(){
+        document.getElementById("copy-msg").classList.remove("copied-box");
+    }, 1000);
+})
+
+speechBtn.addEventListener("click", () => {
+    if (!quoteBtn.classList.contains("loading")) {
+        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+        synth.speak(utterance);
+        setInterval(() => {
+            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
+        }, 10);
+    }
+})
+
+twitterBtn.addEventListener("click", () => {
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+})
+
+quoteBtn.addEventListener("click", randomQuote);
